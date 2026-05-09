6ca1cb3a67ef494a9bb0c9200eb5137b96ff5c03
idealistichacker <>
2026-05-09 15:15:17 +0800

    Fast forward to Exercise:8. Back to green v1.0

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const express = require("express");
-const app = express();
-
-// get the port from env variable
-const PORT = process.env.PORT || 5001;
-
-app.use(express.static("dist"));
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5001
+app.use(express.static('dist'))
 
 const start = async () => {
   await app.listen(PORT)
