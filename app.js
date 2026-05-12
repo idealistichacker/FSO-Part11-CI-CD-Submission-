a00ba763343ea35fd720589ceaf17ea331a17cc8
idealistichacker <>
2026-05-12 11:41:16 +0800

    Exercise:12. Health check --- Render Health Check is error v2.0

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ app.use(express.static('dist'))
 
 // --- 在这里添加测试接口 ---
 app.get('/version', (req, res) => {
-  res.send('Exercise:12. Health check --- Render Health Check is ok v1.0') // 当你下次想测试部署是否生效时，把这里改成 '2'
+  res.send('Exercise:12. Health check --- Render Health Check is error v2.0') // 当你下次想测试部署是否生效时，把这里改成 '2'
 })
 //防止“坏代码”上线导致服务崩溃。
 app.get('/health', (req, res) => {
-  res.send('ok')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
 })
 // -----------------------
 
