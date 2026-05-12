faaee280f481b2c608c774536d0b7db05e270871
idealistichacker <>
2026-05-12 11:55:56 +0800

    Exercise:12. Health check --- Render Health Check is error --- fix eslint Error v2.0

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/version', (req, res) => {
   res.send('Exercise:12. Health check --- Render Health Check is error v2.0') // 当你下次想测试部署是否生效时，把这里改成 '2'
 })
 //防止“坏代码”上线导致服务崩溃。
-app.get('/health', (req, res) => {
+app.get('/health', () => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
 })
